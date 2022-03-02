0af35cf4c8dace60c91729483088d657cbf678c3
Sam Williams <samwilliams281@gmail.com>
2022-03-01 20:35:28 -0500

    version 1

---
+++ assets/js/recent.js
@@ -0,0 +1,55 @@
+var recentCity = document.getElementById("recent-city");
+
+var recentContainer1 = document.getElementById("recent-1");
+var recentContainer2 = document.getElementById("recent-2");
+var recentContainer3 = document.getElementById("recent-3");
+var recentContainer4 = document.getElementById("recent-4");
+var recentContainer5 = document.getElementById("recent-5");
+var recentContainer6 = document.getElementById("recent-6");
+var recentContainer7 = document.getElementById("recent-7");
+var recentContainer8 = document.getElementById("recent-8");
+var recentContainer9 = document.getElementById("recent-9");
+
+var recent1H2 = document.getElementById("recent-1-h2");
+var recent2H2 = document.getElementById("recent-2-h2");
+var recent3H2 = document.getElementById("recent-3-h2");
+var recent4H2 = document.getElementById("recent-4-h2");
+var recent5H2 = document.getElementById("recent-5-h2");
+var recent6H2 = document.getElementById("recent-6-h2");
+var recent7H2 = document.getElementById("recent-7-h2");
+var recent8H2 = document.getElementById("recent-8-h2");
+var recent9H2 = document.getElementById("recent-9-h2");
+
+var cityName = localStorage.getItem("cityName")
+
+recentCity.textContent = cityName.toUpperCase()
+
+recent1H2.textContent = localStorage.getItem("event-1-h2")
+recent2H2.textContent = localStorage.getItem("event-2-h2")
+recent3H2.textContent = localStorage.getItem("event-3-h2")
+recent4H2.textContent = localStorage.getItem("event-4-h2")
+recent5H2.textContent = localStorage.getItem("event-5-h2")
+recent6H2.textContent = localStorage.getItem("event-6-h2")
+recent7H2.textContent = localStorage.getItem("event-7-h2")
+recent8H2.textContent = localStorage.getItem("event-8-h2")
+recent9H2.textContent = localStorage.getItem("event-9-h2")
+
+var recent1Date = document.getElementById("recent-date-1");
+var recent2Date = document.getElementById("recent-date-2");
+var recent3Date = document.getElementById("recent-date-3");
+var recent4Date = document.getElementById("recent-date-4");
+var recent5Date = document.getElementById("recent-date-5");
+var recent6Date = document.getElementById("recent-date-6");
+var recent7Date = document.getElementById("recent-date-7");
+var recent8Date = document.getElementById("recent-date-8");
+var recent9Date = document.getElementById("recent-date-9");
+
+recent1Date.textContent = localStorage.getItem("event-1-date");
+recent2Date.textContent = localStorage.getItem("event-2-date");
+recent3Date.textContent = localStorage.getItem("event-3-date");
+recent4Date.textContent = localStorage.getItem("event-4-date");
+recent5Date.textContent = localStorage.getItem("event-5-date");
+recent6Date.textContent = localStorage.getItem("event-6-date");
+recent7Date.textContent = localStorage.getItem("event-7-date");
+recent8Date.textContent = localStorage.getItem("event-8-date");
+recent9Date.textContent = localStorage.getItem("event-9-date");
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,24 @@
 var concertsContainer = document.getElementById("concerts-container");
 var cityName = document.getElementById("city-name");
 var searchBtn = document.getElementById("search-btn")
+var clearBtn = document.getElementById("clear-btn")
+var searchForm = document.getElementById("search-form")
 
-// event listeners to fetch data from tickemaster api and display on the page
-searchBtn.addEventListener("click", function () {
+var event1 = document.getElementById("event-1");
+var event2 = document.getElementById("event-2");
+var event3 = document.getElementById("event-3");
+var event4 = document.getElementById("event-4");
+var event5 = document.getElementById("event-5");
+var event6 = document.getElementById("event-6");
+var event7 = document.getElementById("event-7");
+var event8 = document.getElementById("event-8");
+var event9 = document.getElementById("event-9");
+
+// event listener to fetch data from tickemaster api and display on the page
+searchForm.addEventListener("submit", function (event) {
+    event.preventDefault()
+    concertsContainer.setAttribute("style", "display: inherent;")
+    localStorage.setItem("cityName", cityName.value);
     var ticketmasterApiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?size=9&city="+ cityName.value +"&apikey=lZue0rCxljAKRpD2FdMAM9O7pIks2mQI";
   fetch(ticketmasterApiUrl)
     .then(function (response) {
@@ -13,22 +28,44 @@ searchBtn.addEventListener("click", function () {
         response.json().then(function (data) {
           console.log(data);
           console.log(data._embedded.events)
-          console.log(data._embedded.events[1].name)
-          console.log(data._embedded.events[1].url)
-          console.log(data._embedded.events[1].images[0].url)
-          console.log(data._embedded.events[1].dates.start.localDate)
+          console.log(data._embedded.events[0].name)
+          console.log(data._embedded.events[0].url)
+          console.log(data._embedded.events[0].images[0].url)
+          console.log(data._embedded.events[0].dates.start.localDate)
 
           var eventsArr = data._embedded.events
-          
-        //   for loop to loop through events, create an anchor element for each and, style
-          for(var i = 0; i < eventsArr.length; i ++){
-              var concert1 = document.createElement("a");
-              concert1.setAttribute("style", "border-radius: 10px; background-color: #00bcd4; color: white; height: 200px; width: 100%;")
-              concert1.href = eventsArr[i].url;
-              concert1.textContent = eventsArr[i].name +" - "+ eventsArr[i].dates.start.localDate
-              concertsContainer.appendChild(concert1);
-          };
-          
+          concertsContainer.innerHTML= ""
+            // for loop to loop through events, create an anchor element for each and, style
+            for(var i = 0; i < eventsArr.length; i ++){
+               var events = document.createElement("a");
+                events.setAttribute("style", "border-radius: 10px; background-color: #06b6d4; color: white; height: 200px; width: 100%; border: 2px solid black;")
+                events.href = eventsArr[i].url;
+                events.textContent = eventsArr[i].name +"  -  "+ eventsArr[i].dates.start.localDate.substring(5,10) +"-"+ eventsArr[0].dates.start.localDate.substring(0,4)
+                events.style.backgroundImage = "url('"+eventsArr[i].images[0].url+"')"
+                concertsContainer.appendChild(events);
+            };
+
+            // stores event names to local storage
+          localStorage.setItem("event-1-h2", eventsArr[0].name) 
+          localStorage.setItem("event-2-h2", eventsArr[1].name) 
+          localStorage.setItem("event-3-h2", eventsArr[2].name) 
+          localStorage.setItem("event-4-h2", eventsArr[3].name) 
+          localStorage.setItem("event-5-h2", eventsArr[4].name) 
+          localStorage.setItem("event-6-h2", eventsArr[5].name) 
+          localStorage.setItem("event-7-h2", eventsArr[6].name) 
+          localStorage.setItem("event-8-h2", eventsArr[7].name) 
+          localStorage.setItem("event-9-h2", eventsArr[8].name)
+            
+            // stores formated dates to local storage
+          localStorage.setItem("event-1-date", eventsArr[0].dates.start.localDate.substring(5,10) +"-"+ eventsArr[0].dates.start.localDate.substring(0,4))
+          localStorage.setItem("event-2-date", eventsArr[1].dates.start.localDate.substring(5,10) +"-"+ eventsArr[1].dates.start.localDate.substring(0,4))
+          localStorage.setItem("event-3-date", eventsArr[2].dates.start.localDate.substring(5,10) +"-"+ eventsArr[2].dates.start.localDate.substring(0,4))
+          localStorage.setItem("event-4-date", eventsArr[3].dates.start.localDate.substring(5,10) +"-"+ eventsArr[3].dates.start.localDate.substring(0,4))
+          localStorage.setItem("event-5-date", eventsArr[4].dates.start.localDate.substring(5,10) +"-"+ eventsArr[4].dates.start.localDate.substring(0,4))
+          localStorage.setItem("event-6-date", eventsArr[5].dates.start.localDate.substring(5,10) +"-"+ eventsArr[5].dates.start.localDate.substring(0,4))
+          localStorage.setItem("event-7-date", eventsArr[6].dates.start.localDate.substring(5,10) +"-"+ eventsArr[6].dates.start.localDate.substring(0,4))
+          localStorage.setItem("event-8-date", eventsArr[7].dates.start.localDate.substring(5,10) +"-"+ eventsArr[7].dates.start.localDate.substring(0,4))
+          localStorage.setItem("event-9-date", eventsArr[8].dates.start.localDate.substring(5,10) +"-"+ eventsArr[8].dates.start.localDate.substring(0,4))
         });
       } else {
         //   alerts need to be converted to modals

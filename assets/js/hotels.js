3f7e4d3ba6795e84f067ae1b0f2c54ffd89c3971
Jessica Santiago <jessie_13084@yahoo.com>
2022-03-09 20:54:59 -0500

    added hotel api

--- assets/js/hotels.js
+++ assets/js/hotels.js
@@ -3,36 +3,26 @@ var cityName = document.getElementById("city-name");
 var searchBtn = document.getElementById("search-btn");
 var searchForm = document.getElementById("search-form");
 
-
-
-
-
-
 // event listener to fetch data from hotels api and display on the page
 searchForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    hotelsContainer.setAttribute("style", "display: inherent;");
-    console.log(cityName.value)
-    localStorage.setItem("cityName", cityName.value);
+  event.preventDefault();
+  hotelsContainer.setAttribute("style", "display: inherent;");
 
-    //var hotelApiUrl =
-     
-
-    fetch("https://hotels4.p.rapidapi.com/locations/v2/search?query=new%20york&locale=en_US&currency=USD", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "hotels4.p.rapidapi.com",
-		"x-rapidapi-key": "a1f1e4f2a3msh6da74de769912dcp109e5ajsn6e99b5c4d1d2"
-	}
-})
-.then(response => {
-	//console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
-      
+  fetch("https://travel-advisor.p.rapidapi.com/locations/v2/auto-complete?query=" + cityName.value + "lang=en_US&units=mi", {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+      "x-rapidapi-key": "a1f1e4f2a3msh6da74de769912dcp109e5ajsn6e99b5c4d1d2"
+    }
+    
+  })
+  .then((response) => response.json())
+      .then((data) => {
+      console.log(data);
+  })
+  .catch(err => {
+    console.error(err);
   });
 
 
- 
+});
